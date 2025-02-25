798ff3308a5837f965375fec9f11a80646a7e1b9
alampf <alanpatlani65@gmail.com>
2025-02-25 12:31:46 -0600

    Pantalla Principal se agrego Existencia y Precio

--- lib/screens/phones_screen.dart
+++ lib/screens/phones_screen.dart
@@ -149,8 +149,17 @@ class _PhoneScreenState extends State<PhoneScreen> {
 
   ListTile oneTile(PhoneModel phone) {
     return ListTile(
+      leading: const Icon(Icons.phone_android),
       title: Text(phone.marca),
-      subtitle: Text(phone.modelo),
+      subtitle: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(phone.modelo),
+          Text('Existencias: ${phone.existencia}'),
+          Text('Precio: \$${phone.precio}'),
+        ],
+      ),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
